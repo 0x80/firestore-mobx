a10f87ca2c972b704cebdfcbfb10dae1331f1a9f
Thijs Koerselman <thijskoerselman@gmail.com>
2020-01-28 12:23:18 +0700

    Fix test

--- src/__test/document.test.ts
+++ src/__test/document.test.ts
@@ -3,7 +3,7 @@ import { ObservableDocument } from "../document";
 test("Create a document", () => {
   const document = new ObservableDocument();
 
-  expect(document.isLoading.get()).toBe(false);
+  expect(document.isLoading).toBe(false);
   expect(document.hasData).toBe(false);
   expect(document.data).toBe(undefined);
 });
@@ -11,7 +11,7 @@ test("Create a document", () => {
 test("Create a document from ref", () => {
   const document = new ObservableDocument();
 
-  expect(document.isLoading.get()).toBe(false);
+  expect(document.isLoading).toBe(false);
   expect(document.hasData).toBe(false);
   expect(document.data).toBe(undefined);
 });
