c121a18a68317f62d07a6e707d11a24cc2ffc63b
Thijs Koerselman <thijskoerselman@gmail.com>
2020-04-25 19:04:05 +0200

    Update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,15 @@
 module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node"
+  roots: ["<rootDir>/src"],
+  testEnvironment: "node",
+  testMatch: [
+    "**/__tests__/**/*.+(ts|tsx|js)",
+    "**/?(*.)+(spec|test).+(ts|tsx|js)",
+  ],
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+  },
+
+  moduleNameMapper: {
+    "~/(.*)": "<rootDir>/src/$1",
+  },
 };
